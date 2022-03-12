f54bd093c63db8a22c032d40a1421eaba3a6b8a7
Tài Nguyễn Hữu <18521361@ms.uit.edu.vn>
2022-03-12 16:04:32 +0700

    mumbai testnet

--- pages/creator-dashboard.jsx
+++ pages/creator-dashboard.jsx
@@ -65,7 +65,7 @@ export default function CreatorDashboard() {
               <img src={nft.image} className="rounded" />
               <div className="p-4 bg-black">
                 <p className="text-2xl font-bold text-white">
-                  Price - {nft.price} Eth
+                  Price - {nft.price} ETH
                 </p>
               </div>
             </div>
